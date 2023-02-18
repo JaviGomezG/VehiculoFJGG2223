4c098d4b394e4069f5dbe2ebf69d8e867aabf6e9
PC-Casa <PC-Casa@192.168.1.14>
2023-02-18 19:49:28 +0100

    Este es mi primer commit FJGG2223

---
+++ src/vehiculo/Main.java
@@ -0,0 +1,41 @@
+
+package vehiculo;
+
+/**
+ *
+ * @author IES Aguadulce
+ */
+public class Main {
+    
+      public static void main(String[] args) {
+        VehiculoGomezGameroFranciscoJavier2223 miVehiculoGomezGameroFranciscoJavier2223;
+        int stockActual;
+        
+        miVehiculoGomezGameroFranciscoJavier2223 = new VehiculoGomezGameroFranciscoJavier2223("Seat",18000,100);
+        operativaVehiculosGomezGameroFranciscoJavier2223(miVehiculoGomezGameroFranciscoJavier2223, 50); 
+    }
+
+    public static void operativaVehiculosGomezGameroFranciscoJavier2223(VehiculoGomezGameroFranciscoJavier2223 miVehiculoGomezGameroFranciscoJavier2223, int cantidad) {
+        int stockActual;
+        try
+        {
+            System.out.println("Venta de Vehiculos");
+            miVehiculoGomezGameroFranciscoJavier2223.vender(20);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al vender");
+        }
+        try
+        {
+            System.out.println("Compra de Vehiculos");
+            miVehiculoGomezGameroFranciscoJavier2223.comprar(100);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al comprar");
+        }
+        stockActual = miVehiculoGomezGameroFranciscoJavier2223.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+}
+    
